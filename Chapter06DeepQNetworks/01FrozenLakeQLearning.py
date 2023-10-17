e708070837a3136b381daef845e4aefd04ccf6a4
Christoph Kempkes <88708549+gvtsch@users.noreply.github.com>
2023-10-17 14:50:56 +0200

    Add files via upload

--- Chapter06DeepQNetworks/01FrozenLakeQLearning.py
+++ Chapter06DeepQNetworks/01FrozenLakeQLearning.py
@@ -17,7 +17,7 @@ class Agent():
         action = self.env.action_space.sample()
         old_state = self.state
         new_state, reward, is_done, _ = self.env.step(action)
-        self.state = self.env.reset() if is_done else new_state
+        self.state = self.env.reset() if is_done else new_state 
         return old_state, action, reward, new_state
     
     def best_value_and_action(self, state):
